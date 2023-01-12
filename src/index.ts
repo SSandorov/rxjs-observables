9d8705a672ba2095f8f01d080c5732a69517ad65
ssandorov <49904591+SSandorov@users.noreply.github.com>
2023-01-12 16:56:55 +0100

    Observer y Subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+
+// Esta es una manera alternativa de hacer el subscribe(). Ya definimos con
+// anterioridad las funciones opcionales del subscribe()
+const observer: Observer<any> = {
+    next: value => console.log('siguiente [next]: ', value),
+    error: error => console.warn('error [obs]: ', error),
+    complete: () => console.info('completado [obs]')
+}
 
 // Es buena práctica poner el símbolo del dólar al final del nombre de la
 // variable para saber que es un observable
@@ -13,6 +21,10 @@ const obs$ = new Observable<string>(subs => {
     subs.next('Hola');
     subs.next('Mundo');
 
+    // Forzamos un error
+    // const a = undefined;
+    // a.nombre = 'Fernando';
+
     // para dejar de emitir 
     subs.complete();
 
@@ -22,8 +34,16 @@ const obs$ = new Observable<string>(subs => {
 });
 
 // Para que el observable se ejecute debe tener una suscripción
-obs$.subscribe( console.log );
+// obs$.subscribe( 
+//     // Dentro del subscribe tenemos 3 argumentos opcionales
+//     // 1. el next que nos devuelve la emisión del Observable
+//     valor => console.log('next: ', valor),
+//     error => console.warn('error: ', error),
+//     () => console.info('Completado')
+//  );
 
+// Empleando el observer
+obs$.subscribe(observer);
 
 
 
